7038a9eee9aa13cc9af65f54ca3d4ee7df9a368a
Tetsu Kinomura <tetsu99soccer@gmail.com>
2022-02-18 10:26:55 +0900

    :tada: Feat: メモリマップの取得とファイルへの書き込み

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -1,11 +1,169 @@
 #include <Uefi.h>
 #include <Library/UefiLib.h>
+#include <Library/UefiBootServiceLib.h>
+#include <Library/PrintLib.h>
+#include <Protocol/LoadedImage.h>
+#include <Protocol/SimpleFileSystem.h>
+#include <Protocol/DiskIo2.h>
+#include <Protocol/BlockIo.h>
+
+struct MemoryMap
+{
+    UINTN buffer_size;
+    VOID* buffer;
+    UINTN map_size;
+    UINTN map_key;
+    UINTN descriptor_size;
+    UINT32 descriptor_version
+};
+
+// UEFIの機能を使ってメモリマップを取得する
+// UEFIには
+//   ブートサービス: OSを起動するために必要な機能を提供する
+//   ランタイムサービス: OSの起動前後どちらにでも使える機能を提供する
+// の2つがある.
+EFI_STATUS GetMemoryMap(struct MemoryMap* map)
+{
+    if (map->buffer == NULL) {
+        return EFI_BUFFER_TOO_SMALL;
+    }
+
+    map->map_size = map->buffer_size;
+
+    // ブートサービスからメモリマップを取得する
+    // 呼び出し時点のメモリマップを取得し、引数で指定されたメモリ領域に書き込む.
+    return gBS->getMemoryMap(
+        &map->map_size;
+        (EFI_MEMORY_DESCRIPTOR*)map->buffer,
+        &map->map_key,
+        &map->descriptor_size,
+        &map->descriptor_version
+    );
+}
+
+const CHAR16* GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
+  switch (type) {
+    case EfiReservedMemoryType: return L"EfiReservedMemoryType";
+    case EfiLoaderCode: return L"EfiLoaderCode";
+    case EfiLoaderData: return L"EfiLoaderData";
+    case EfiBootServicesCode: return L"EfiBootServicesCode";
+    case EfiBootServicesData: return L"EfiBootServicesData";
+    case EfiRuntimeServicesCode: return L"EfiRuntimeServicesCode";
+    case EfiRuntimeServicesData: return L"EfiRuntimeServicesData";
+    case EfiConventionalMemory: return L"EfiConventionalMemory";
+    case EfiUnusableMemory: return L"EfiUnusableMemory";
+    case EfiACPIReclaimMemory: return L"EfiACPIReclaimMemory";
+    case EfiACPIMemoryNVS: return L"EfiACPIMemoryNVS";
+    case EfiMemoryMappedIO: return L"EfiMemoryMappedIO";
+    case EfiMemoryMappedIOPortSpace: return L"EfiMemoryMappedIOPortSpace";
+    case EfiPalCode: return L"EfiPalCode";
+    case EfiPersistentMemory: return L"EfiPersistentMemory";
+    case EfiMaxMemoryType: return L"EfiMaxMemoryType";
+    default: return L"InvalidMemoryType";
+  }
+}
+
+EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file)
+{
+    CHAR8 buf[256];
+    UINTN len;
+
+    CHAR8* header = "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n"
+    len = AsciiStrLen(header)
+    file->Write(file, &len, header);
+
+    Print(L"map->buffer = %08lx, map->map_size = %08lx\n", map->buffer, map->map_size);
+
+    EFI_PHYSICAL_ADDRESS iter;
+    int i;
+    for (
+        iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i = 0;
+        iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
+        iter += map->descriptor_size, i++
+    ) {
+        EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)iter
+
+        len = AsciiSPrint(
+            buf,
+            sizeof(buf),
+            "%u, %x, %-ls, %08lx, %lx, %lx\n",
+            i,
+            desc->Type,
+            GetMemotyTypeUnicode(desc->Type),
+            desc->PhysicalStart,
+            desc->NumberOfPages,
+            desc->Attribute & 0xffffflu
+        );
+    }
+
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root)
+{
+    EFI_LOADED_IMAGE_PROTOCOL* loaded_iamge;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs;
+
+    gBS->OpenProtocol(
+        image_handle,
+        &gEfiLoadedImageProtocolGuid,
+        (VOID**)&loaded_image,
+        image_handle,
+        NULL,
+        EIF_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL
+    );
+
+    gBS->OpenProtocol(
+        image_handle->DeviceHandle,
+        &gEfiLoadedImageProtocolGuid,
+        (VOID**)&fs,
+        image_handle,
+        NULL,
+        EIF_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL
+    );
+
+    fs->OpenVolume(fs, root)
+
+    return EFI_SUCCESS;
+}
+
 
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
-    EFI_SYSTEM_TABLE *system_table
+    EFI_SYSTEM_TABLE* system_table
 ) {
     Print(L"Hello, My MikanOS!\n");
+
+    CHAR8 memmap_buf[1024 * 4 * 4]
+    struct MemoryMap memmap = {sizeof(mammap_buf), memmap_buf, 0, 0, 0, 0}
+    status = GetMemoryMap(&memmap);
+    if (status != EFI_SUCCESS) {
+        Print(L"GetMemoryMap failed!!!!");
+    }
+
+    EFI_FILE_PROTOCOL* root_dir;
+    status = OpenRootDir(image_handle, &root_dir);
+    if (status != EFI_SUCCESS) {
+        Print(L"OpenRootDir failed!!!!");
+    }
+
+    EFI_FILE_PROTOCOL* memmap_file;
+    root_dir->Open(
+        root_dir,
+        &memmpa_file,
+        L"\\memmap",
+        EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE,
+        0
+    );
+
+    status = SaveMemoryMap(&memmap, memmap_file);
+    if (status != EFI_SUCCESS) {
+        Print(L"SaveMemoryMap failed!!!!");
+    }
+    memmap_file->Close(memmap_file);
+
+    Print(L"All done\n");
+
     while (1);
     return EFI_SUCCESS;
 }
