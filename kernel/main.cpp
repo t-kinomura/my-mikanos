a98ec322a3feb64ac21a7ca017f326ae6c3fea91
Tetsu Kinomura <tetsu99soccer@gmail.com>
2022-02-21 08:02:32 +0900

    :lipstick: 色は白でいい

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -3,7 +3,7 @@
 extern "C" void KernelMain(uint64_t frame_buffer_base, uint64_t frame_buffer_size) {
     uint8_t* frame_buffer = reinterpret_cast<uint8_t*>(frame_buffer_base);
     for(uint64_t i = 0; i < frame_buffer_size; ++i) {
-        frame_buffer[i] = i % 256;
+        frame_buffer[i] = 255;
     }
     // __asm__(): インラインアセンブラ
     // hlt: cpuを停止させ省電力な状態にする. 割り込みがあると再開するけどこのプログラムでは割り込みは起こらない.
