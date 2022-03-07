40cad3c610990ce53d48da152a2d0a2d336787fc
Tetsu Kinomura <tetsu99soccer@gmail.com>
2022-03-07 22:28:42 +0900

    :bug: Fix: kernelの読み込み処理のメモリサイズ計算

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -2,13 +2,15 @@
 #include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/PrintLib.h>
-#include  <Library/MemoryAllocationLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/BaseMemoryLib.h>
 #include <Protocol/LoadedImage.h>
 #include <Protocol/SimpleFileSystem.h>
 #include <Protocol/DiskIo2.h>
 #include <Protocol/BlockIo.h>
 #include <Guid/FileInfo.h>
 #include "frame_buffer_config.hpp"
+#include "elf.hpp"
 
 struct MemoryMap
 {
@@ -197,6 +199,32 @@ void Halt(void) {
     while(1) __asm__("hlt");
 }
 
+void CalcLoadAddressRange(Elf64_Ehdr* ehdr, UINT64* first, UINT64* last)
+{
+    Elf64_Phdr* phdr = (Elf64_Phdr*)((UINT64)ehdr + ehdr->e_phoff); // e_phoffはプログラムヘッダーのオフセットを表す項目
+    *first = MAX_UINT64;
+    *last = 0;
+    for (Elf64_Half i = 0; i < ehdr->e_phnum; i++) { // e_phnumは要素数を表す項目
+        if (phdr[i].p_type != PT_LOAD) continue;
+        *first = MIN(*first, phdr[i].p_vaddr);
+        *last = MAX(*last, phdr[i].p_vaddr + phdr[i].p_memsz);
+    }
+}
+
+void CopyLoadSegments(Elf64_Ehdr* ehdr) // ehdr: コピー元のメモリの先頭アドレス
+{
+    Elf64_Phdr* phdr = (Elf64_Phdr*)((UINT64)ehdr + ehdr->e_phoff);
+    for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i) {
+        if (phdr[i].p_type != PT_LOAD) continue;
+
+        UINT64 segm_in_file = (UINT64)ehdr + phdr[i].p_offset;
+        CopyMem((VOID*)phdr[i].p_vaddr, (VOID*)segm_in_file, phdr[i].p_filesz);
+    
+        UINTN remain_bytes = phdr[i].p_memsz - phdr[i].p_filesz;
+        SetMem((VOID*)(phdr[i].p_vaddr + phdr[i].p_filesz), remain_bytes, 0);
+    }
+}
+
 
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
@@ -299,22 +327,37 @@ EFI_STATUS EFIAPI UefiMain(
     EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
     UINTN kernel_file_size = file_info->FileSize;
 
-    EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
-    status = gBS->AllocatePages(
-        AllocateAddress,
-        EfiLoaderData,
-        (kernel_file_size + 0xfff) / 0x1000,
-        &kernel_base_addr
-    );
+    // カーネルファイルを一時領域に読み込む. kernel_bufferはその領域の先頭アドレス.
+    VOID* kernel_buffer;
+    status = gBS->AllocatePool(EfiLoaderData, kernel_file_size, &kernel_buffer);
     if(EFI_ERROR(status)) {
+        Print(L"failed to allocate pool: %r\n", status);
+        Halt();
+    }
+    status = kernel_file->Read(kernel_file, &kernel_file_size, kernel_buffer);
+    if(EFI_ERROR(status)) {
+        Print(L"error: %r\n", status);
+        Halt();
+    }
+
+    // コピー先のメモリ領域の確保
+    Elf64_Ehdr* kernel_ehdr = (Elf64_Ehdr*)kernel_buffer;
+    UINT64 kernel_first_addr, kernel_last_addr;
+    CalcLoadAddressRange(kernel_ehdr, &kernel_first_addr, &kernel_last_addr);
+
+    UINTN num_pages = (kernel_last_addr - kernel_first_addr + 0xfff) / 0x1000;
+    status = gBS->AllocatePages(AllocateAddress, EfiLoaderData, num_pages, &kernel_first_addr);
+    if (EFI_ERROR(status)) {
         Print(L"failed to allocate pages: %r\n", status);
         Halt();
     }
 
-    status = kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
-    Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
+    CopyLoadSegments(kernel_ehdr);
+    Print(L"Kernel: 0x%0lx - 0x%0lx\n", kernel_first_addr, kernel_last_addr);
+
+    status = gBS->FreePool(kernel_buffer);
     if(EFI_ERROR(status)) {
-        Print(L"error: %r\n", status);
+        Print(L"failed to free pool: %r\n", status);
         Halt();
     }
 
@@ -334,7 +377,7 @@ EFI_STATUS EFIAPI UefiMain(
         }
     }
 
-    UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
+    UINT64 entry_addr = *(UINT64*)(kernel_first_addr + 24);
 
     struct FrameBufferConfig config = {
         (UINT8*)gop->Mode->FrameBufferBase,
